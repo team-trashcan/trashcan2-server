0a3c4a60904d831277d7f2da6ad2d4ccd307134d
Wurstkatze <wurstkatze@posteo.de>
2025-01-22 14:05:21 +0100

    save test

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import config from "./config";
 import app from "./app";
+import saveToJson from "./Shared/saveToJson";
 
 const server = async (): Promise<void> => {
+  console.log("[DEBUG] testing file saving");
+  saveToJson("test", { someData: "someValue" });
   app.listen(config.port, () => {
     console.log(`Server is listening on port ${config.port}`);
   });
