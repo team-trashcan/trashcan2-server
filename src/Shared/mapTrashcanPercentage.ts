9b6dd97309ac93c28f2767f34138808d6c46c0ac
Wurstkatze <wurstkatze@posteo.de>
2025-05-14 14:28:33 +0200

    adjusted percentage range

--- src/Shared/mapTrashcanPercentage.ts
+++ src/Shared/mapTrashcanPercentage.ts
@@ -1,7 +1,11 @@
+import appConfig from '../config'
+
 function mapValue(value: number, inMin: number, inMax: number, outMin: number, outMax: number) {
   return ((value - inMin) / (inMax - inMin)) * (outMax - outMin) + outMin
 }
 
 export default function mapTrashcanPercentage(value: number, unclamped?: boolean) {
-  return unclamped ? mapValue(value, 50, 250, 100, 0) : Math.max(0, Math.min(100, mapValue(value, 50, 250, 100, 0)))
+  return unclamped
+    ? mapValue(value, appConfig.percentage.full, appConfig.percentage.empty, 100, 0)
+    : Math.max(0, Math.min(100, mapValue(value, 50, 250, 100, 0)))
 }
